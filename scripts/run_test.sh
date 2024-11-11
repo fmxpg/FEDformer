ed35b34e446a54766c71a53da9d40d3f7ad90f54
CaoYue <2535876596@qq.com>
2024-11-11 15:11:56 +0800

    2024-11-11 1st

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -5,19 +5,42 @@ cd ..
 for model in FEDformer Autoformer Informer Transformer
 do
 
-for preLen in 96 192 336 720
-do
+#for preLen in 96 192 336 720
+#do
 
+#python -u run.py \
+# --is_training 1 \
+# --root_path ./dataset/weather/ \
+# --data_path weather.csv \
+# --task_id weather \
+# --model $model \
+# --data custom \
+# --features S \
+# --seq_len 96 \
+# --label_len 48 \
+# --pred_len $preLen \
+# --e_layers 2 \
+# --d_layers 1 \
+# --factor 3 \
+# --enc_in 1 \
+# --dec_in 1 \
+# --c_out 1 \
+# --des 'Exp' \
+# --itr 1  # edited
+
+for preLen in 24 #36 48 60
+do
+# illness
 python -u run.py \
  --is_training 1 \
- --root_path ./dataset/weather/ \
- --data_path weather.csv \
- --task_id weather \
+ --root_path ./dataset/illness/ \
+ --data_path national_illness.csv \
+ --task_id ili \
  --model $model \
  --data custom \
  --features S \
- --seq_len 96 \
- --label_len 48 \
+ --seq_len 36 \
+ --label_len 18 \
  --pred_len $preLen \
  --e_layers 2 \
  --d_layers 1 \
@@ -26,7 +49,7 @@ python -u run.py \
  --dec_in 1 \
  --c_out 1 \
  --des 'Exp' \
- --itr 1  # edited
-
+ --itr 1
 done
+
 done
